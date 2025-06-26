fe0dd323f5ffb607055baf0be7b64e39c1b7f736
Jessica Cafezeiro <155641678+jessicawolfie@users.noreply.github.com>
2025-06-26 20:42:52 -0300

    updates

--- Game/Game_v3.py
+++ Game/Game_v3.py
@@ -3,6 +3,13 @@
 # Programação orientada a objetos
 
 import random
+from os import system, name
+
+def limpa_tela():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 board = ['''
 
